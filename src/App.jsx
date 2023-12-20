c62a47bb9af3fb6d788d61b3a968811536964886
Race Osuna <119973475+RaceOsuna@users.noreply.github.com>
2023-12-19 21:13:36 -0700

    Feat: setup firestore

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+
+import { initializeApp } from "firebase/app";
+import { collection, doc, addDoc, setDoc, getFirestore, onSnapshot, getDocs} from "firebase/firestore"
+import { firebaseConfig } from './firebaseConfig';
 
 function App() {
-  const [count, setCount] = useState(0)
 
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const myCollection = collection(db, 'reservations')
+
+const [resData, setResData] = useState([])
+const [resCount, setResCount] = useState(resData.length)
+
+
+const getData = async() => {
+  const snaps = await getDocs(myCollection)
+  let reservs = [];
+  snaps.forEach(snap => {
+    reservs.push(snap.data())
+  })
+  setResData(reservs)
+  setResCount(reservs.length)
+}
+
+useEffect(() => {
+  getData()
+}, [resCount])
+
+
+console.log(resData)
+console.log(resCount)
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     
     </>
   )
 }
---
+++ src/firebaseConfig.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBkFqXiuP-7HL6UfZNhcpgRmI2FA1pr7KI",
+  authDomain: "vacanseat-c5589.firebaseapp.com",
+  projectId: "vacanseat-c5589",
+  storageBucket: "vacanseat-c5589.appspot.com",
+  messagingSenderId: "996937360300",
+  appId: "1:996937360300:web:39c3525c39dc81d6989009",
+  measurementId: "G-XRFMC7JN4C"
+};
+
+export { firebaseConfig }
+// Initialize Firebase
