f5b309264a2d03361289991f0c865444cbb726d2
Race Osuna <119973475+RaceOsuna@users.noreply.github.com>
2024-01-04 13:08:25 -0700

    Feat: get real time data

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 
 import { initializeApp } from "firebase/app";
-import { collection, doc, setDoc, getFirestore, getDocs, deleteDoc} from "firebase/firestore"
+import { collection, doc, setDoc, getFirestore, getDocs, deleteDoc, onSnapshot} from "firebase/firestore"
 import { firebaseConfig } from './firebaseConfig';
 import Reservations from './Reservations/Reservations';
 import Layout from './Layout/Layout';
@@ -15,28 +15,23 @@ const myCollection = collection(db, 'reservations')
 
 const [resData, setResData] = useState([])
 const [resCount, setResCount] = useState(resData.length)
-
 const [selectedDate, setSelectedDate] = useState(new Date(new Date().toLocaleDateString()).toISOString().split('T')[0])
-
 const [showForm, setShowForm] = useState(false)
 
-const getData = async() => {
-  const snaps = await getDocs(myCollection)
-  
-  let reservs = [];
-  snaps.forEach(snap => {
-    reservs.push([snap.data(), snap.id])
+useEffect(() => {
+  onSnapshot(myCollection, (snapshot) => {
+    let reservs = []
+    snapshot.forEach(item => {
+      let task = {
+        ...item.data(),
+        docId: item.id
+      }
+      reservs = [task, ...reservs]
+    })
+    setResData(reservs)
   })
-
-  let reducedReservs = reservs.reduce((acc, curr) => {
-    curr[0].docId = curr[1]
-    acc.push(curr[0])
-    return acc
-  }, [])
-
-  setResData(reducedReservs)
-  setResCount(reservs.length)
-}
+}, [])
+console.log(resCount)
 
 const addReservation = async(event, data) => {
   event.preventDefault()
@@ -48,20 +43,13 @@ const addReservation = async(event, data) => {
 
 const deleteReservation = async(id) => {
   await deleteDoc(doc(myCollection, id));
-  setResCount(prev => prev -= 1)
- 
 }
-
-useEffect(() => {
-  getData()
-}, [resCount])
 
   return (
     <div className='app'>
       <Layout selectedDate={selectedDate} setSelectedDate={setSelectedDate} setShowForm={setShowForm} showForm={showForm}>
         {!showForm && <Reservations resData={resData} selectedDate={selectedDate} deleteReservation={deleteReservation}/>}
         {showForm && <Form addReservation={addReservation} selectedDate={selectedDate} />}
-        {/* <ToggleEdit></ToggleEdit> */}
       </Layout>
     </div>
   )
--- src/Reservations/ResCard.jsx
+++ src/Reservations/ResCard.jsx
@@ -7,16 +7,7 @@ export default function ResCard({res, deleteReservation}) {
   const {edit, setEdit} = useContext(EditContext)
   
   const phoneFormat = (input) => {
-    // if(typeof(input) !== 'string') input = input.toString()
-    if(input.length === 10){
       return input.replace(/(\d{3})(\d{3})(\d{4})/, "$1-$2-$3");
-    } else if(input.length < 10) {
-      return 'was not supplied enough numbers please pass a 10 digit number'
-    } else if(input.length > 10) {
-      return 'was supplied too many numbers please pass a 10 digit number'
-    }else{
-      return 'something went wrong'
-    }
   }
   
   const formattedPhone = phoneFormat(res.phoneNumber)
