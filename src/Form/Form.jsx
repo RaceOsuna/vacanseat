be8070a96a2e64b9b6a8d74dcd8ae31863eb3105
Race Osuna <119973475+RaceOsuna@users.noreply.github.com>
2024-01-05 16:02:43 -0700

    Feat: form error handling

--- src/Form/Form.jsx
+++ src/Form/Form.jsx
@@ -15,7 +15,14 @@ export default function Form({addReservation, selectedDate, resToEdit, setResToE
     notes: ''
   })
 
-  const [formError, setFormError] = useState(false)
+  const [formError, setFormError] = useState({
+    date: selectedDate,
+    name: '',
+    time: '',
+    partySize: '',
+    phoneNumber: '',
+    notes: ''
+  })
 
   const reservalableTimes = timeSlots.map((time, index) => (
     <option key={time} value={time === 'select time' ? '' : `${time}-${index + 1}`}>{time}</option>
@@ -29,13 +36,22 @@ export default function Form({addReservation, selectedDate, resToEdit, setResToE
       }
     } )
   }
-  console.log(formError)
+  
   const handleSubmit = (event) => {
     event.preventDefault()
     const keys = Object.keys(formData).filter(key => key !== 'notes')
     if (keys.some(key => formData[key] === '' || formData.phoneNumber.length !== 10)) {
-      setFormError(true)
-      alert('fill out all input fields')
+      keys.forEach(key => {
+        if (formData[key] === '') {
+          return setFormError(prev => ({...prev, [key]: 'error'}))
+        }
+        if (key === 'phoneNumber' && formData.phoneNumber !== 10) {
+          return setFormError(prev => ({...prev, [key]: 'error'}))
+        }
+        if (key !== '') {
+          return setFormError(prev => ({...prev, [key]: ''}))
+        }
+      })
       return
     }
 
@@ -79,14 +95,23 @@ export default function Form({addReservation, selectedDate, resToEdit, setResToE
       <label htmlFor='date'></label>
       <input type='date' name='date' value={formData.date} onChange={handleChange} required />
 
+      {formError.name && <div className='error-message'>
+        <p>please provide a name</p>
+        </div>}
       <label htmlFor="name"></label>
       <input type='text' name='name' value={formData.name} placeholder='name' onChange={handleChange} required />
 
+      {formError.time && <div className='error-message'>
+        <p>please select a time</p>
+        </div>}
       <label htmlFor='time'></label>
       <select name='time' value={formData.time} onChange={handleChange} required >
         {reservalableTimes}
       </select>
 
+      {formError.partySize && <div className='error-message'>
+        <p>please specify party size</p>
+        </div>}
       <label htmlFor='partySiize'></label>
       <select name='partySize' value={formData.partySize} onChange={handleChange} required >
         <option value=''>party size</option>
@@ -104,6 +129,9 @@ export default function Form({addReservation, selectedDate, resToEdit, setResToE
         <option value='12'>12</option>
       </select>
 
+      {formError.phoneNumber && <div className='error-message'>
+        <p>please provide a ten digit phone number</p>
+        </div>}
       <label htmlFor="phoneNumber" ></label>
       <input type="tel" name='phoneNumber' value={formData.phoneNumber} placeholder='phone number' minLength={10} maxLength={10} onChange={handleChange} required/>
 
